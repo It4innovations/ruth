8ac6b314c040de9afea17f8c8a75c8b18cdbf030
Martin Surkovsky <martin.surkovsky@vsb.cz>
2021-10-20 10:35:24 +0200

    added input data preprocessing

---
+++ preprocess_benchmark_data.py
@@ -0,0 +1,20 @@
+
+import os
+import click
+import pandas as pd
+from ruth.preprocessing import *
+
+@click.command()
+@click.argument("in_path", type=click.Path(exists=True))
+@click.option("--out", type=str, default="out.pickle")
+def preprocess(in_path, out):
+    out_path = os.path.abspath(out)
+    df = pd.read_csv(in_path, delimiter=';')
+    df = assign_border(df)
+    df = compute_osm_nodes_id(df)
+    df = prepare_vehicle_state(df)
+    df.to_pickle(out_path)
+    print(f"preprocessed data stored in: {out_path}")
+
+if __name__ == "__main__":
+    preprocess()
---
+++ ruth/preprocessing.py
@@ -0,0 +1,66 @@
+"""Preprocessing of input data."""
+
+import numpy as np
+import pandas as pd
+import osmnx as ox
+from dataclasses import fields
+from datetime import timedelta
+from tqdm import tqdm
+
+from .vehicle import Vehicle
+from .data.geopoint import GeoPoint
+from .utils import get_map
+
+
+def assign_border(df: pd.DataFrame) -> pd.DataFrame:
+    # TODO: identify the border based on start and end GPS
+    df["border_id"] = "CZ010"
+    return df
+
+
+def compute_osm_nodes_id(df: pd.DataFrame) -> pd.DataFrame:
+    required = ["lat_from", "lon_from", "lat_to", "lon_to", "border_id"]
+    assert all(map(lambda item: item in df.columns, required)), \
+      f"To compute osm nodes id the dataframe must contain these items: {required}."
+
+    tqdm.pandas(desc="computing OSM nodes")
+    df[["origin_node", "dest_node"]] = df.progress_apply(_compute_osm_nodes,
+                                                         axis=1,
+                                                         result_type="expand")
+
+    df["active"] = df[["origin_node", "dest_node"]].apply(_is_active, axis=1)
+
+    return df
+
+
+def prepare_vehicle_state(df: pd.DataFrame) -> pd.DataFrame:
+    # rename the columns
+    df[["time_offset", "frequency"]] = \
+      df[["start_offset_s", "route_step_s"]].applymap(lambda seconds: timedelta(seconds=seconds))
+
+    # set segment position
+    df[["start_index", "start_distance_offset"]] = (0, 0.0)
+
+    # set default route
+    df["osm_route"] = np.empty((len(df), 0)).tolist()  # empty list for each route
+
+    return df[list(map(lambda field: field.name, fields(Vehicle)))]
+
+
+def _compute_osm_nodes(row):
+    lat_from, lon_from = row["lat_from"], row["lon_from"]
+    lat_to, lon_to = row["lat_to"], row["lon_to"]
+
+    start_ = GeoPoint(lat_from, lon_from).point()
+    end_ = GeoPoint(lat_to, lon_to).point()
+
+    routing_map = get_map(row["border_id"])
+    starting_node = ox.distance.nearest_nodes(
+        routing_map.network, start_.x, start_.y)
+    destination_node = ox.distance.nearest_nodes(
+        routing_map.network, end_.x, end_.y)
+
+    return (starting_node, destination_node)
+
+def _is_active(row):
+    return row["origin_node"] != row["dest_node"]
