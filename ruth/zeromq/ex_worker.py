6a0131036d9b074c6a2bdbbbca269edd323d34a5
Jakub Beránek <jakub.beranek@vsb.cz>
2023-05-17 14:24:11 +0200

    Add ZeroMQ implementation of a kernel provider

--- ruth/zeromq/ex_worker.py
+++ ruth/zeromq/ex_worker.py
@@ -1,3 +1,5 @@
+import logging
+
 from src import worker
 import click
 
@@ -7,9 +9,15 @@ import click
 @click.option('--port', default=5560, type=int, help='Client port')
 @click.option('--map', help='Graph filepath')
 def main(address: str, port: int, map: str):
+    logging.info(f"Starting alternatives worker. Connecting to {address}:{port} with map {map}")
     w = worker.Worker(map=map, address=address, port=port)
     w.run()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(name)s:%(levelname)-4s %(message)s",
+        datefmt="%d-%m-%Y %H:%M:%S",
+    )
     main()
