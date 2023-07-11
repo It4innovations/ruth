47649f2423568ebd20abc0a6e492875d2b2b3780
mar23 <jan.martinovic@vsb.cz>
2023-07-11 09:09:18 +0200

    revert

--- ruth/simulator/__init__.py
+++ ruth/simulator/__init__.py
@@ -1,4 +1,4 @@
 from .routeranking import RouteRankingAlgorithms
-from .singlenode2 import Simulator as SingleNodeSimulator
+from .singlenode import Simulator as SingleNodeSimulator
 from .simulation import Simulation, SimSetting
 from .common import load_vehicles, save_vehicles
