6f789b6f62f77172e1c753ebccb697fd255d05cc
Jakub Beránek <berykubik@gmail.com>
2023-08-11 12:39:24 +0200

    Send map to alternatives provider
    It is unused for the local providers, but required for the distributed provider to load the map and also to re-map IDs.

--- ruth/simulator/kernels.py
+++ ruth/simulator/kernels.py
@@ -1,28 +1,39 @@
-import json
 from typing import List, Optional, Tuple
 
 from .segment import Route
+from ..data.map import Map
 from ..vehicle import Vehicle
+from ..zeromq.src.client import Message
 
 AlternativeRoutes = List[Route]
 
+
 # Alternatives
 class AlternativesProvider:
-    """
-    Provides implementation of computing alternatives.
-    For a given list of vehicles and parameter `k`, computes a list of routes for each vehicle.
-    """
-    def compute_alternatives(self, vehicles: List[Vehicle], k: int) -> List[Optional[AlternativeRoutes]]:
+    def load_map(self, map: Map):
+        """
+        Loads updated information from the passed map.
+        """
+        pass
+
+    def compute_alternatives(self, map: Map, vehicles: List[Vehicle], k: int) -> List[
+        Optional[AlternativeRoutes]]:
+        """
+        Provides implementation of computing alternatives.
+        For a given list of vehicles and parameter `k`, computes a list of routes for each vehicle.
+        """
         raise NotImplementedError
 
 
 class FastestPathsAlternatives(AlternativesProvider):
-    def compute_alternatives(self, vehicles: List[Vehicle], k: int) -> List[Optional[AlternativeRoutes]]:
+    def compute_alternatives(self, map: Map, vehicles: List[Vehicle], k: int) -> List[
+        Optional[AlternativeRoutes]]:
         return [vehicle.k_fastest_paths(k) for vehicle in vehicles]
 
 
 class ShortestPathsAlternatives(AlternativesProvider):
-    def compute_alternatives(self, vehicles: List[Vehicle], k: int) -> List[Optional[AlternativeRoutes]]:
+    def compute_alternatives(self, map: Map, vehicles: List[Vehicle], k: int) -> List[
+        Optional[AlternativeRoutes]]:
         return [vehicle.k_shortest_paths(k) for vehicle in vehicles]
 
 
@@ -31,15 +42,33 @@ class ZeroMQDistributedAlternatives(AlternativesProvider):
         from ..zeromq.src.client import Client
         self.client = Client(port=port)
 
-    def compute_alternatives(self, vehicles: List[Vehicle], k: int) -> List[Optional[AlternativeRoutes]]:
-        od_paths = [(*v.next_routing_od_nodes, k) for v in vehicles]
-        inputs = [json.dumps(x).encode() for x in od_paths]
-        result = self.client.compute(inputs)
-        return result
+    def load_map(self, map: Map):
+        """
+        Loads updated information from the passed map.
+        """
+        # TODO: implement with broadcast
+        self.client.compute([Message(kind="load-map", data=map.hdf5_file_path)])
+
+    def compute_alternatives(self, map: Map, vehicles: List[Vehicle], k: int) -> List[
+        Optional[AlternativeRoutes]]:
+        messages = [Message(kind="alternatives", data={
+            "start": map.osm_to_hdf5_id(v.next_routing_od_nodes[0]),
+            "destination": map.osm_to_hdf5_id(v.next_routing_od_nodes[1]),
+            "max_routes": k
+        }) for v in vehicles]
+        # logging.info(f"Sending alternatives to distributed worker: {messages}")
+        results = self.client.compute(messages)
+        # logging.info(f"Response from worker: {results}")
+        remapped_routes = [
+            [[map.hdf5_to_osm_id(node_id) for node_id in route] for route in result["routes"]]
+            for result in results
+        ]
+        return remapped_routes
 
 
 VehicleWithPlans = Tuple[Vehicle, AlternativeRoutes]
 VehicleWithRoute = Tuple[Vehicle, Route]
+
 
 # Route selection
 class RouteSelectionProvider:
@@ -47,6 +76,7 @@ class RouteSelectionProvider:
     Provides implementation of route selection.
     For a given list of alternatives (k per vehicle), select the best route for each vehicle.
     """
+
     def select_routes(self, route_possibilities: List[VehicleWithPlans]) -> List[VehicleWithRoute]:
         raise NotImplementedError
 
@@ -55,5 +85,6 @@ class FirstRouteSelection(RouteSelectionProvider):
     """
     Selects the first route for each car.
     """
+
     def select_routes(self, route_possibilities: List[VehicleWithPlans]) -> List[VehicleWithRoute]:
         return [(vehicle, routes[0]) for (vehicle, routes) in route_possibilities]
