aad42f17c988810cba7664160cfec86b85f1110f
Sofia Michailidu <sofia@michailidu.cz>
2023-10-27 11:20:00 +0200

    move removing inactive vehicles from queues to a separate method

--- ruth/simulator/queues.py
+++ ruth/simulator/queues.py
@@ -10,8 +10,13 @@ class QueuesManager:
     def add_to_queue(self, vehicle: Vehicle):
         self.queues[(vehicle.current_node, vehicle.next_node)].append(vehicle)
 
+    def remove_inactive_vehicle(self, vehicle: Vehicle):
+        for queue in self.queues.values():
+            if vehicle in queue:
+                queue.remove(vehicle)
+
     def remove_vehicle(self, vehicle: Vehicle, node_from, node_to):
         queue = self.queues[(node_from, node_to)]
-        if len(queue) != 0:
-            popped_vehicle = queue.popleft()
-            assert popped_vehicle == vehicle
+        assert len(queue) != 0
+        popped_vehicle = queue.popleft()
+        assert popped_vehicle == vehicle
