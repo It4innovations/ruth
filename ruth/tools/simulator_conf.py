99a7fe11af88a7cce9bd5efae09324c78a1cfc26
Sofia Michailidu <sofia@michailidu.cz>
2024-01-29 11:53:42 +0100

    remove ptdr path from config params

--- ruth/tools/simulator_conf.py
+++ ruth/tools/simulator_conf.py
@@ -37,9 +37,6 @@ class CommonArgs(CommonArgsInner):
     speeds_path: Optional[str] = field(serializer=lambda x: '' if x is None else x,
                                        deserializer=lambda x: None if x == '' else x,
                                        default=None)
-    ptdr_path: Optional[str] = field(serializer=lambda x: '' if x is None else x,
-                                     deserializer=lambda x: None if x == '' else x,
-                                     default=None)
     out: str = "simulation-record.pickle"
     seed: Optional[int] = None
     walltime: Optional[timedelta] = field(rename="walltime-s",
