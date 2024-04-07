01d08c78b33e9e1631d624bbd8f3add08ea78d53
Roman Machacek <69751521+User3574@users.noreply.github.com>
2024-04-07 13:54:14 +0200

    Update bench
    Add run as a background process for the bench

--- ruth/zeromq/bench.py
+++ ruth/zeromq/bench.py
@@ -68,24 +68,8 @@ def run(workers: int,
     try_to_kill                 = experimental, tries to kill workers after simulation is computed
     spawn_workers_at_main_node  = experimental, spawns workers at the same node where main process is located
 
-    Problem:
-    Killing workers may result in error due to rights (connected to cluster library)
-
-    Example Usage:
-    WORK_DIR = Path(os.getcwd()).absolute()
-    WORKER_DIR = WORK_DIR / str(sys.argv[1])
-    ENV_PATH = os.environ["VIRTUAL_ENV"]
-    MODULES = [
-        "Python/3.10.8-GCCcore-12.2.0",
-        "GCC/12.2.0",
-        "SQLite/3.39.4-GCCcore-12.2.0",
-        "HDF5/1.14.0-gompi-2022b",
-        "CMake/3.24.3-GCCcore-12.2.0",
-        "Boost/1.81.0-GCC-12.2.0"
-    ]
-    CONFIG_FILE = "/mnt/proj2/open-27-41/simulator/ruth/config.json"
-    EVKIT_PATH = "/mnt/proj2/open-27-41/simulator/evkit"
-    hosts = get_slurm_nodes()
+    Potential Issue:
+    -   Killing workers may result in error due to rights (connected to cluster library)
     """
     # Find open port
     port = find_free_port()
@@ -173,149 +157,77 @@ def run(workers: int,
         print(f"Workers per node: {workers}, Nodes: {len(hosts) - 1}, computation time: {duration}")
         if try_to_kill == True:
             cluster_data.kill()
-        return RunResult(repeat=0, output=output, duration=duration)
+
+        # Save to file and return info about run
+        result = RunResult(repeat=0, output=output, duration=duration)
+        json_data = json.dumps(results)
+        df = pd.read_json(json_data)
+        df.to_csv(f"{WORKER_DIR}/results.csv", index=False)
+        return result
 
     except Exception as e:
         print(e)
 
 
-def bench(hosts: List[str], WORKER_DIR) -> RunResult:
+def bench(workers: List[int],
+          hosts: List[str],
+          OUTPUT_DIR,
+          CONFIG_FILE: str,
+          EVKIT_PATH: str,
+          MODULES: List[str],
+          ENV_PATH,
+          try_to_kill:bool,
+          spawn_workers_at_main_node:bool,
+          repeats:int) -> RunResult:
+    """
+    Run the benchmark on multiple host(s), nodes.
+    workers                     = amount of workers
+    WORKER_DIR                  = where the workers will be stored
+    CONFIG_FILE                 = path to config file (from ruth)
+    EVKIT_PATH                  = path to evkit
+    MODULES                     = modules used in order to spawn workers
+    ENV_PATH                    = path to the environment
+    try_to_kill                 = experimental, tries to kill workers after simulation is computed
+    spawn_workers_at_main_node  = experimental, spawns workers at the same node where main process is located
+    repeats                     = number of amounts the experiment is repeated
+
+    Potential Issue:
+    -   Building the workers inside run, build them inside bench and then just run them
+    """
+
     CLIENT_ADDRESS = hosts[0]
     print(f'Client is running at: {CLIENT_ADDRESS}')
-    CONFIG_FILE = "/mnt/proj2/open-27-41/simulator/ruth/config.json"
-
-    # Start main
-    target_dir = Path("/mnt/proj2/open-27-41/simulator/evkit")
-    target_dir.mkdir(parents=True, exist_ok=True)
-    target_process = start_process(
-        commands=[
-            f"cargo build --release --features alternatives,rpath --manifest-path /mnt/proj2/open-27-41/simulator/evkit/Cargo.toml"],
-        workdir=str(target_dir),
-        pyenv=str(ENV_PATH),
-        modules=MODULES,
-        hostname=CLIENT_ADDRESS,
-        name=f"target"
-    )
-
-    # Wait until the process end
-    print("Building the worker")
-    while is_running(target_process.pid):
-        time.sleep(0.25)
-    print("Worker built")
-
     results = []
-    repeats = 3
-    workers = [32]  # [1, 1, 2, 4, 8, 16, 32] #[1, 2, 4, 8, 16, 32, 64]
-    for r in range(repeats):
-        total = 0
 
-        # Find two open ports
+    # Preprocess workers based on if we try to kill them
+    if try_to_kill is False:
+        active_workers = workers[0] # 1
+        updated_workers = [workers[0]] # [1]
+        for idx in range(1, len(workers)):
+            w = workers[idx]
+            new_workers = w - active_workers
+            updated_workers.append(new_workers)
+            active_workers += new_workers
+        workers = updated_workers
+
+    # Main loop for benchmark
+    for r in range(repeats):
         port = find_free_port()
         management_port = port + 1
 
         for w in workers:
-            total += w
+            WORKER_DIR = OUTPUT_DIR / str(w)
             try:
-                cluster_data = Cluster(str(WORK_DIR))
-                print(f"Saving to: {WORKER_DIR}")
-                print(f"Environment in: {ENV_PATH}")
-                print(f"Running client at: {CLIENT_ADDRESS}")
-                output = WORKER_DIR / f"workers_{total}" / f"repeat_{r}"
-
-                for n in range(1, len(hosts)):
-                    HOST_ADDRESS = hosts[n]
-
-                    for i in range(w):
-                        print(f"Creating worker {i} at host {HOST_ADDRESS}")
-                        worker_dir = output / f"node_{n}" / f"worker_{i}"
-                        worker_dir.mkdir(parents=True, exist_ok=True)
-                        worker_process = start_process(
-                            commands=[
-                                f"{target_dir}/target/release/worker run {CLIENT_ADDRESS}:{port} {CLIENT_ADDRESS}:{management_port}"],
-                            workdir=str(worker_dir),
-                            env={"RUST_LOG": "debug"},
-                            pyenv=str(ENV_PATH),
-                            modules=MODULES,
-                            hostname=HOST_ADDRESS,
-                            name=f"worker_{i}"
-                        )
-                        time.sleep(1)
-                        # cluster_data.add(worker_process)
-
-                # Wait for workers to be spawned
-                time.sleep(60)
-
-                # Start main
-                main_dir = output / f"main"
-                main_dir.mkdir(parents=True, exist_ok=True)
-                main_process = start_process(
-                    commands=[f"ruth-simulator-conf --config-file={CONFIG_FILE} run"],
-                    workdir=str(main_dir),
-                    pyenv=str(ENV_PATH),
-                    env={"port": port},
-                    modules=MODULES,
-                    hostname=CLIENT_ADDRESS,
-                    name=f"main"
-                )
-                cluster_data.add(main_process)
-
-                # Start
-                start = time.time()
-
-                # Wait until the process end
-                print("Runing the main computation")
-                while is_running(main_process.pid):
-                    time.sleep(0.25)
-                print("Main computation ended#")
-
-                end = time.time()
-                duration = end - start
-                print(f"Workers per node: {w}, Nodes: {len(hosts) - 1}, computation time: {duration}")
-
-                results.append(RunResult(repeat=r, output=output, duration=duration))
+                result = run(w, hosts, WORKER_DIR, CONFIG_FILE, EVKIT_PATH, MODULES, ENV_PATH, try_to_kill, spawn_workers_at_main_node)
+                results.append(result)
                 time.sleep(10)
 
             except KeyboardInterrupt:
                 continue
 
-    return results
-
-
-if __name__ == "__main__":
-    WORK_DIR = Path(os.getcwd()).absolute()
-    WORKER_DIR = WORK_DIR / str(sys.argv[1])
-    ENV_PATH = os.environ["VIRTUAL_ENV"]
-    MODULES = [
-        "Python/3.10.8-GCCcore-12.2.0",
-        "GCC/12.2.0",
-        "SQLite/3.39.4-GCCcore-12.2.0",
-        "HDF5/1.14.0-gompi-2022b",
-        "CMake/3.24.3-GCCcore-12.2.0",
-        "Boost/1.81.0-GCC-12.2.0"
-    ]
-    CONFIG_FILE = "/mnt/proj2/open-27-41/simulator/ruth/config.json"
-    EVKIT_PATH = "/mnt/proj2/open-27-41/simulator/evkit"
-    hosts = get_slurm_nodes()
-    workers = 32
-    try_to_kill = False
-    spawn_workers_at_main_node = False
-
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(module)s:%(levelname)s %(message)s")
-
-    result = run(
-        workers=workers,
-        hosts=hosts,
-        WORKER_DIR=WORKER_DIR,
-        CONFIG_FILE=CONFIG_FILE,
-        EVKIT_PATH=EVKIT_PATH,
-        MODULES=MODULES,
-        ENV_PATH=ENV_PATH,
-        try_to_kill=try_to_kill,
-        spawn_workers_at_main_node=spawn_workers_at_main_node
-    )
-    # result = bench(nodes, WORKER_DIR)
-
     # Save
-    json_data = json.dumps(result)
+    json_data = json.dumps(results)
     df = pd.read_json(json_data)
-    df.to_csv(f"{WORKER_DIR}/results.csv", index=False)
+    df.to_csv(f"{OUTPUT_DIR}/results.csv", index=False)
+
+    return results
