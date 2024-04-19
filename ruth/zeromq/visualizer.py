7ccc9cd7e699ef2d3ce4b0d727b33c7f73ee85ee
Roman Machacek <69751521+User3574@users.noreply.github.com>
2024-04-19 11:35:50 +0200

    Update visualizer.py

--- ruth/zeromq/visualizer.py
+++ ruth/zeromq/visualizer.py
@@ -4,15 +4,16 @@ import click
 import matplotlib.pyplot as plt
 import os
 
-from ..tools import simulator
+from ruth.tools import simulator
 
 
 @click.command()
 @click.option('--path_to_pickle', help='Benchmark directory')
 @click.option('--workers')
 @click.option('--nodes')
-def visualize(path_to_pickle, workers, nodes):
-    plot_types = ['n_active', 'duration', 'violin_dur_per_steps', 'hist_duration_steps']
+@click.option('--map_size')
+def visualize(path_to_pickle, workers, nodes, map_size):
+    plot_types = ['n_active', 'duration_per_step', 'violin_dur_per_steps', 'hist_duration_steps']
     path, filename = os.path.split(path_to_pickle)
     fig = plt.figure(figsize=(15, 15))
     simulation = simulator.Simulation.load(path_to_pickle)
@@ -25,20 +26,42 @@ def visualize(path_to_pickle, workers, nodes):
     # df_parts = df_parts.loc[:, df_parts.columns != 'selected_routes_followup']
     df = pd.concat([df, df_parts], axis=1)
 
+    # Convert to seconds
+    df['duration'] = df['duration'].dt.total_seconds()
+    df['alternatives'] = df['alternatives'] / 1000
+    df['advance_vehicle'] = df['advance_vehicle'] / 1000
+    df['selected_routes'] = df['selected_routes'] / 1000
+
     for i, plot_type in enumerate(plot_types):
         ax = fig.add_subplot(2, 2, i + 1)
         if plot_type == 'n_active':
-            seaborn.lineplot(df[['n_active', 'need_new_route']], ax=ax).set(title=f'{plot_type}')
-        elif plot_type == 'duration':
+            if 'need_new_route' in df:
+                seaborn.lineplot(df[['n_active', 'need_new_route']], ax=ax).set(title=f'{plot_type}')
+                ax.set_ylabel("Amount of cars")
+                ax.set_xlabel('Step')
+            else:
+                seaborn.lineplot(df[['n_active']], ax=ax).set(title=f'{plot_type}')
+                ax.set_ylabel("Amount of cars")
+                ax.set_xlabel('Step')
+        elif plot_type == 'duration_per_step':
             seaborn.violinplot(x=df['duration'], ax=ax).set(title=f'{plot_type}')
+            ax.set_xlabel('Duration [s]')
+            ax.set_ylabel('Frequency')
         elif plot_type == 'violin_dur_per_steps':
             seaborn.violinplot(ax=ax, data=df[['alternatives', 'advance_vehicle', 'selected_routes']]).set(title=f'{plot_type}')
+            ax.set_xlabel('Function')
+            ax.set_ylabel('Duration [s]')
         elif plot_type == 'hist_duration_steps':
             seaborn.histplot(ax=ax, data=df[['alternatives', 'advance_vehicle', 'selected_routes']], bins=100).set(title=f'{plot_type}')
+            ax.set_xlabel('Duration [s]')
+            ax.set_ylabel('Count')
         else:
             raise "Bad name for plot type"
-    plt.suptitle(f'Several figures for time consuming parts of simulation\n{nodes} nodes with {workers} workers per node')
-    plt.savefig(path + 'visual.png')
+    plt.suptitle(f'Several figures for time consuming parts of simulation\n{nodes} nodes with {workers} workers per node for {map_size} map')
+    plt.savefig(path + '/visual.png')
+
+    # Amount of cars
+    print('Amount of processed cars: ', df['n_active'].sum())
 
     # Get times in seconds
     simulation_time = simulation.duration.total_seconds()
@@ -55,7 +78,7 @@ def visualize(path_to_pickle, workers, nodes):
     plt.clf()
     (df_parts / measured_time).plot.bar(title=f'Computation distribution \n{nodes} nodes with {workers} workers per node')
     plt.xticks(rotation=30)
-    plt.savefig(path + 'distribution.png')
+    plt.savefig(path + '/distribution.png')
 
 
 if __name__ == '__main__':
