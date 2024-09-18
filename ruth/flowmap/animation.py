87414638a5afe61799d99c5a2c28132d9747ede4
Pavlina Smolkova <smo0117@vsb.cz>
2024-09-18 12:28:28 +0200

    integrate traffic-flow-map repo and speed-profiles repo into ruth

---
+++ ruth/flowmap/animation.py
@@ -0,0 +1,399 @@
+from datetime import datetime, timedelta
+from time import time
+from os import path
+from abc import ABC, abstractmethod
+
+import logging
+import matplotlib as mpl
+import numpy as np
+from matplotlib import pyplot as plt, animation
+from shapely import LineString
+from tqdm import tqdm
+from collections import defaultdict
+
+from .flowmapframe.zoom import plot_graph_with_zoom
+from .flowmapframe.plot import get_node_coordinates, WidthStyle
+from .flowmapframe.speeds import (plot_routes as plot_routes_speeds,
+                                  get_color_bar_info as get_color_bar_info_speeds)
+from .flowmapframe.plot import (plot_routes as plot_routes_densities,
+                                get_color_bar_info as get_color_bar_info_densities)
+
+from ruth.utils import TimerSet
+from ruth.simulator import Simulation
+
+from .ax_settings import AxSettings
+from .zoom import get_zoom
+from .input import preprocess_data, calculate_computation_by_simulation_time, prepare_dataframe, \
+    calculate_active_vehicles_in_time
+
+
+def load_file_content(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
+def round_timedelta(td):
+    seconds = td.total_seconds()
+    rounded_seconds = round(seconds)
+    return timedelta(seconds=rounded_seconds)
+
+
+def timedeltas_to_timestamps(timedeltas: np.array, start_time: datetime) -> np.array:
+    return np.array([start_time + td for td in timedeltas])
+
+
+def load_description(path):
+    description = load_file_content(path)
+    if len(description) > 990:
+        print('Warning: description is too long. The end might not be visible in the animation.')
+    return description
+
+
+class SimulationAnimator(ABC):
+    def __init__(self, simulation_path, fps, save_path, frame_start, frames_len, width_modif, title, description,
+                 description_path, length, divide, max_width_count, plot_cars, zoom, gif):
+        self.simulation_path = simulation_path
+        self.simulation = None
+        self.fps = fps
+        self.save_path = save_path
+        self.frame_start = frame_start
+        self.frames_len = frames_len
+        self.width_modif = width_modif
+        self.title = title
+        self.length = length
+        self.speed = None
+        self.divide = divide
+        self.max_width_count = max_width_count
+        self.plot_cars = plot_cars
+        self.zoom = zoom
+        self.generate_gif = gif
+
+        self.description = None
+        if description:
+            self.description = description
+        elif description_path:
+            self.description = load_description(description_path)
+
+        self.ts = TimerSet()
+
+    @property
+    def interval(self):
+        return self.speed / self.fps
+
+    @property
+    @abstractmethod
+    def colorbar_title(self):
+        pass
+
+    def plot_cbar(self):
+        cmap, norm = self.get_color_bar_info()
+        cbar = plt.colorbar(mappable=mpl.cm.ScalarMappable(norm=norm, cmap=cmap), ax=self.ax_map,
+                            shrink=0.4, label=self.colorbar_title(), pad=0.0)
+        ticklabs = cbar.ax.get_yticklabels()
+        cbar.ax.set_yticklabels(ticklabs, fontsize=15)
+        label = cbar.ax.yaxis.label
+        label.set_fontsize(15)
+        return cbar
+
+    def set_simulation(self, simulation):
+        self.simulation = simulation
+
+    def run(self):
+        with self.ts.get("data loading"):
+            logging.info('Loading simulation data...')
+            self._load_data()
+            logging.info('Simulation data loaded.')
+
+        with self.ts.get("data preprocessing"):
+            logging.info('Preprocessing data...')
+            self._preprocess_data()
+            logging.info('Data preprocessed.')
+
+        self._set_ax_settings_if_zoom()
+
+        with self.ts.get("base map preparing"):
+            logging.info('Preparing base map...')
+            self._prepare_base_map()
+            logging.info('Base map prepared.')
+
+        with self.ts.get("create animation"):
+            logging.info('Creating animation...')
+            self._create_animation()
+            logging.info('Animation created.')
+
+        print()
+        for k, v in self.ts.collect().items():
+            print(f'{k}: {v} ms')
+
+    def _load_data(self):
+        if self.simulation is None:
+            self.simulation = Simulation.load(self.simulation_path)
+        self.g = self.simulation.routing_map.network
+        self.sim_history = self.simulation.history.to_dataframe()
+
+        simulation_length = self.simulation.get_length()
+        simulation_seconds = simulation_length.total_seconds()
+        self.speed = int(simulation_seconds / self.length)
+
+    def _preprocess_data(self):
+        df = prepare_dataframe(self.sim_history, self.speed, self.fps)
+        self.active_vehicles_in_time = calculate_active_vehicles_in_time(df)
+        preprocessed_data = preprocess_data(df, self.g, self.divide)
+        self.segments = preprocessed_data.segments
+        self.number_of_vehicles = preprocessed_data.number_of_vehicles
+        self.number_of_finished_vehicles_in_time = preprocessed_data.number_of_finished_vehicles_in_time
+        self.total_meters_driven_in_time = preprocessed_data.total_meters_driven_in_time
+        self.total_simulation_time_in_time_s = preprocessed_data.total_simulation_time_in_time_s
+        timestamps = [seg.timestamp for seg in preprocessed_data.timed_segments]
+        min_timestamp = min(timestamps)
+        max_timestamp = max(timestamps)
+
+        if self.max_width_count is None:
+            self.max_width_count = max([max(seg.counts) for seg in preprocessed_data.timed_segments])
+        self.timestamp_from = min_timestamp + self.frame_start
+        self.num_of_frames = max_timestamp - self.timestamp_from + 1  # + 1 to include the last frame
+        self.num_of_frames = min(int(self.frames_len), self.num_of_frames) if self.frames_len else self.num_of_frames
+
+        self.timed_seg_dict = defaultdict(list)
+        for seg in preprocessed_data.timed_segments:
+            self.timed_seg_dict[seg.timestamp].append(seg)
+
+        self.computation_by_simulation_time = None
+        si_df = self.simulation.steps_info_to_dataframe()
+        if 'simulation_offset' in si_df.columns and 'duration' in si_df.columns:
+            self.computation_by_simulation_time = \
+                calculate_computation_by_simulation_time(si_df,
+                                                         self.simulation.setting.departure_time,
+                                                         max_timestamp,
+                                                         self.speed,
+                                                         self.fps)
+
+    def _set_ax_settings_if_zoom(self):
+        if self.zoom:
+            print('Use the zoom button to choose an area that will be zoomed in in the animation.')
+            print('Close the window when satisfied with the selection.')
+            self.ax_map_settings = get_zoom(self.g, self.segments)
+
+    def _get_stats_text(self, timestamp: int):
+        simulation_time_formatted = round_timedelta(self.total_simulation_time_in_time_s[timestamp])
+        total_km_driven = round(self.total_meters_driven_in_time[timestamp] / 1000, 2)
+        text = f"Total driving time (sum of all cars): {simulation_time_formatted}\n" \
+               f"Total KMs driven: {total_km_driven} km\n"
+        if self.computation_by_simulation_time is not None:
+            computation_time = timedelta(milliseconds=self.computation_by_simulation_time[timestamp])
+            text += f"Computation time: {computation_time}\n"
+
+        return text
+
+    def _get_finished_vehicles_text(self, timestamp: int):
+        return \
+            f'Finished vehicles: {self.number_of_finished_vehicles_in_time[timestamp]} / {self.number_of_vehicles}\n' \
+            f'Active vehicles: {self.active_vehicles_in_time[timestamp]}'
+
+    def _prepare_base_map(self):
+        mpl.use('Agg')
+
+        self.fig, self.ax_map = plt.subplots()
+        plot_graph_with_zoom(self.g, self.ax_map, secondary_sizes=[1, 0.7, 0.5, 0.3])
+
+        if self.zoom:
+            self.ax_map_settings.apply(self.ax_map)
+
+        size = self.fig.get_size_inches()
+        new_size = 20
+        size[1] = size[1] * new_size / size[0]
+        size[0] = new_size
+        self.fig.set_size_inches(size)
+
+        plt.title(self.title, fontsize=40)
+        self.time_text = plt.figtext(
+            0.3,
+            0.09,
+            datetime.utcfromtimestamp(self.timestamp_from * 1000 * self.interval // 10 ** 3),
+            ha='right',
+            fontsize=20)
+
+        self.finished_vehicles_text = plt.figtext(
+            0.7,
+            0.08,
+            self._get_finished_vehicles_text(self.timestamp_from),
+            ha='left',
+            fontsize=15)
+
+        if self.description:
+            txt = plt.figtext(
+                0.15,
+                0.08,
+                self.description,
+                ha='left',
+                va='top',
+                fontsize=10,
+                wrap=True)
+            txt._get_wrap_line_width = lambda: 1000
+
+        self.stats_text = plt.figtext(
+            0.7,
+            0.03,
+            self._get_stats_text(self.timestamp_from))
+
+        self.plot_cbar()
+
+        self.ax_traffic = self.ax_map.twinx()
+        self.ax_map_settings = AxSettings(self.ax_map)
+
+    def _create_animation(self):
+        anim = animation.FuncAnimation(
+            plt.gcf(),
+            self._animate(),
+            interval=75,
+            frames=self.num_of_frames,
+            repeat=False
+        )
+
+        timestamp = round(time() * 1000)
+        filetype = 'gif' if self.generate_gif else 'mp4'
+        anim.save(path.join(self.save_path, f'{str(timestamp)}-rt.{filetype}'), writer='ffmpeg', fps=self.fps)
+
+    def _animate(self):
+        car_coordinates = []
+        progress_bar = None
+
+        def step(i):
+            nonlocal car_coordinates, progress_bar
+            if progress_bar is None:
+                progress_bar = tqdm(total=self.num_of_frames, desc='Creating animation', unit='frame', leave=True)
+            self.ax_traffic.clear()
+            self.ax_map_settings.apply(self.ax_traffic)
+            self.ax_traffic.axis('off')
+
+            timestamp = self.timestamp_from + i
+            if i % 5 * 60 == 0:
+                self.time_text.set_text(datetime.utcfromtimestamp(timestamp * 1000 * self.interval // 10 ** 3))
+
+            self.finished_vehicles_text.set_text(self._get_finished_vehicles_text(timestamp))
+            self.stats_text.set_text(self._get_stats_text(timestamp))
+
+            segments = self._plot_routes(timestamp)
+
+            if self.plot_cars:
+                if len(car_coordinates) == 3:
+                    car_coordinates.pop(0)
+                xp = []
+                yp = []
+                for segment in segments:
+                    if segment.cars_offsets is not None:
+                        x, y = self.get_cars_xy(segment.node_from.id, segment.node_to.id, segment.cars_offsets)
+                        xp.append(x)
+                        yp.append(y)
+                alphas = [1, 0.75, 0.5]
+                car_coordinates.append((xp, yp))
+                for coords, alpha in zip(reversed(car_coordinates), alphas):
+                    self.ax_traffic.scatter(coords[0], coords[1], facecolors='none', edgecolors='black', alpha=alpha)
+
+            progress_bar.update(1)
+            if i == self.num_of_frames - 1:
+                progress_bar.close()
+
+        return step
+
+    @abstractmethod
+    def _plot_routes(self, timestamp):
+        pass
+
+    @abstractmethod
+    def get_color_bar_info(self):
+        pass
+
+    def get_cars_xy(self, node_from, node_to, offsets):
+        x, y = get_node_coordinates(self.g, node_from, node_to)
+
+        line = LineString(zip(x, y))
+        edge_length = self.g[node_from][node_to][0]['length']
+
+        offsets = np.interp(offsets, [0, edge_length], [0, line.length])
+        point = line.interpolate(offsets)
+        return point.x, point.y
+
+
+class SimulationVolumeAnimator(SimulationAnimator):
+    def __init__(self, simulation_path, fps, save_path, frame_start, frames_len, width_modif, title, description,
+                 description_path, length, divide, max_width_count, plot_cars, zoom, gif, width_style="BOXED"):
+        super().__init__(
+            simulation_path,
+            fps,
+            save_path,
+            frame_start,
+            frames_len,
+            width_modif,
+            title,
+            description,
+            description_path,
+            length,
+            divide,
+            max_width_count,
+            plot_cars,
+            zoom,
+            gif
+        )
+        self.width_style = WidthStyle[width_style]
+
+    def _plot_routes(self, timestamp):
+        segments = self.timed_seg_dict[timestamp]
+        nodes_from = [seg.node_from.id for seg in segments]
+        nodes_to = [seg.node_to.id for seg in segments]
+        vehicle_counts = [seg.counts for seg in segments]
+
+        plot_routes_densities(
+            self.g,
+            ax=self.ax_traffic,
+            nodes_from=nodes_from,
+            nodes_to=nodes_to,
+            densities=vehicle_counts,
+            max_width_density=self.max_width_count,
+            width_modifier=self.width_modif,
+            width_style=self.width_style
+        )
+        return segments
+
+    def get_color_bar_info(self):
+        return get_color_bar_info_densities(0, self.max_width_count)
+
+    def colorbar_title(self):
+        return "Number of vehicles"
+
+
+class SimulationSpeedsAnimator(SimulationAnimator):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def _plot_routes(self, timestamp):
+        segments = self.timed_seg_dict[timestamp]
+        nodes_from = [seg.node_from.id for seg in segments]
+        nodes_to = [seg.node_to.id for seg in segments]
+        vehicle_counts = [seg.counts for seg in segments]
+        speeds = [seg.speeds for seg in segments]
+
+        plot_routes_speeds(
+            self.g,
+            ax=self.ax_traffic,
+            nodes_from=nodes_from,
+            nodes_to=nodes_to,
+            densities=vehicle_counts,
+            speeds=speeds,
+            max_width_density=self.max_width_count,
+            width_modifier=self.width_modif
+        )
+        return segments
+
+    def get_color_bar_info(self):
+        return get_color_bar_info_speeds()
+
+    def colorbar_title(self):
+        return "Speed (kph)"
+
+    def plot_cbar(self):
+        cbar = super().plot_cbar()
+        ticklabs = cbar.ax.get_yticklabels()
+        ticklabs[-1] = ""
+        cbar.ax.set_yticklabels(ticklabs)
+        return cbar
---
+++ ruth/flowmap/app.py
@@ -0,0 +1,153 @@
+import os
+import click
+import pathlib
+import platform
+from ruth.simulator import Simulation
+from .flowmapframe.plot import WidthStyle
+from .analysis import create_simulations_comparison
+from .time_unit import TimeUnit
+from .animation import SimulationVolumeAnimator, SimulationSpeedsAnimator
+from .info import SimulationInfo, get_real_time
+
+
+def set_path():
+    # simulations generated on Linux can't be loaded on Windows and vice versa without changing the path
+    if platform.system() == "Windows":
+        pathlib.PosixPath = pathlib.WindowsPath
+    elif platform.system() == "Linux":
+        pathlib.WindowsPath = pathlib.PosixPath
+
+
+@click.group()
+def cli():
+    pass
+
+
+animation_options = {
+    'fps': {'default': 25, 'type': int, 'help': "Set video frames per second.", 'show_default': True},
+    'save_path': {'default': '', 'help': "Path to the folder for the output video."},
+    'frame_start': {'default': 0, 'type': int, 'help': "Number of frames to skip before plotting."},
+    'frames_len': {'type': int, 'help': "Number of frames to plot"},
+    'width_modif': {'default': 10, 'type': click.IntRange(2, 200, clamp=True), 'show_default': True,
+                    'help': "Adjust width."},
+    'title': {'default': '', 'help': "Set video title"},
+    'description': {'default': '',
+                    'help': 'Description to be added to the video. Overrides `description_path` option.'},
+    'description_path': {'default': '', 'help': "Path to the file with description to be added to the video. Only "
+                                                "used if `description` option is not set"},
+    'length': {'default': 30, 'type': int, 'help': "Video length in seconds.", 'show_default': True},
+    'divide': {'default': 2, 'type': int, 'help': "Into how many parts will each segment be split.",
+               'show_default': True},
+    'max_width_count': {'default': None, 'type': int, 'help': "Number of vehicles that corresponds to the maximum "
+                                                              "width of the segment. If not specified, it will be set"
+                                                              " dynamically according to the data.",
+                        'show_default': True},
+    'plot_cars': {'is_flag': True, 'help': "Visualize cars on the map."},
+    'zoom': {'is_flag': True, 'help': "Choose zoom manually."},
+    'gif': {'is_flag': True, 'help': 'Generate as a gif instead of mp4.'}
+}
+
+
+def click_animation_options(function):
+    for option, value in animation_options.items():
+        function = click.option(f'--{option}', **value)(function)
+    return function
+
+
+def click_animation_common(function):
+    function = click.argument('simulation_path')(function)
+    function = click_animation_options(function)
+    return function
+
+
+@cli.command()
+@click_animation_common
+@click.option(
+    '--width-style',
+    type=click.Choice([el.name for el in WidthStyle]),
+    default='BOXED',
+    help="Choose style of width plotting"
+)
+def generate_volume_animation(**kwargs):
+    simulation_animator = SimulationVolumeAnimator(**kwargs)
+    simulation_animator.run()
+
+
+@cli.command()
+@click_animation_common
+def generate_speeds_animation(**kwargs):
+    simulation_animator = SimulationSpeedsAnimator(**kwargs)
+    simulation_animator.run()
+
+
+@cli.command()
+@click.argument('simulation-path', type=click.Path(exists=True))
+@click.option(
+    '--time-unit',
+    type=click.Choice([el.name for el in TimeUnit]),
+    default='seconds',
+    help="Choose time unit for the output",
+    show_default=True
+)
+@click.option(
+    '--minute',
+    type=int,
+    default=None,
+    help="Set if you want to get more detailed info about nth minute of the simulation. Takes longer to calculate",
+    show_default=True
+)
+@click.option(
+    '--status-at-point',
+    type=float,
+    default=None,
+    help="Set if you want to get info about the status of the simulation at a given state of completion (0-1). Takes "
+         "longer to calculate",
+    show_default=True
+)
+def get_info(simulation_path, time_unit, minute, status_at_point):
+    sim = Simulation.load(simulation_path)
+
+    time_unit = TimeUnit.from_str(time_unit)
+
+    real_time = get_real_time(sim, time_unit)
+    print(f'Real time duration: {real_time} {time_unit.name.lower()}.')
+
+    simulation_info = None
+
+    if minute is not None:
+        simulation_info = SimulationInfo(sim)
+        simulation_info.print_info(minute)
+
+    if status_at_point is not None:
+        if simulation_info is None:
+            simulation_info = SimulationInfo(sim)
+        simulation_info.print_status_at_point(status_at_point)
+
+
+@cli.command()
+@click.argument('input-dir', type=click.Path(exists=True))
+@click.option('--output-dir', '-o', default='', help="Path to the folder for the output files.")
+@click.option(
+    '--interval',
+    type=int,
+    default=5,
+    help="Time interval in minutes",
+    show_default=True
+)
+def get_comparison_csv(input_dir, output_dir, interval):
+    pickles = []
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".pickle"):
+            pickles.append(os.path.join(input_dir, filename))
+        else:
+            continue
+    create_simulations_comparison(pickles, output_dir, interval)
+
+
+def main():
+    cli()
+
+
+if __name__ == '__main__':
+    set_path()
+    main()
